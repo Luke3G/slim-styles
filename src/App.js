e66c690e73737fac942bfe7eb26c11358c719bed
luke3me <lucaseganter18@gmail.com>
2022-02-10 19:37:04 -0700

    put on react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="slimstyles">
+      <div className='container-fluid'>
+        <div className='row'>
+        <div className='col-md-12'>
+          <h1>Hello</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
