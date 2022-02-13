a53ba7cbcda4a4491f971312ea03e9304facfe83
Luke Ganter <lucaseganter18@gmail.com>
2022-02-12 22:37:21 -0700

    added all photos centerd text and put dummy text for haircuts

--- src/App.js
+++ src/App.js
@@ -7,39 +7,42 @@ function App() {
       <div className='container-fluid'>
         <div className='row'>
           <div className='header'>
-            <h1>Slim Styls</h1></div>
+            <h1>Slim Styles</h1></div>
         </div>
       </div>
       <div className='saying'>
         <h4>At Slim Styles we are here to please customers we do many cuts, dyes, extions and much more. </h4>
+        <h4>Are Location is 1**** E 10*th Ave </h4>
+        <h4>We Are Open Monday-Saturday From 8:00am to 6:00pm</h4>
       </div>
 
-      <img src='https://i.guim.co.uk/img/media/2d1b194684c17e9cbbc9b5d045f09d316bc91a88/0_132_1920_1919/master/1920.jpg?width=700&quality=85&auto=format&fit=max&s=11c74d5cb0ed244693e7d58c0251abda' width={300}></img>
-      <p>Perm </p>
+      <img src='https://i.guim.co.uk/img/media/2d1b194684c17e9cbbc9b5d045f09d316bc91a88/0_132_1920_1919/master/1920.jpg?width=700&quality=85&auto=format&fit=max&s=11c74d5cb0ed244693e7d58c0251abda' width={300}></img> <img src='https://i.guim.co.uk/img/media/2d1b194684c17e9cbbc9b5d045f09d316bc91a88/0_132_1920_1919/master/1920.jpg?width=700&quality=85&auto=format&fit=max&s=11c74d5cb0ed244693e7d58c0251abda' width={300}></img> <img src='https://i.guim.co.uk/img/media/2d1b194684c17e9cbbc9b5d045f09d316bc91a88/0_132_1920_1919/master/1920.jpg?width=700&quality=85&auto=format&fit=max&s=11c74d5cb0ed244693e7d58c0251abda' width={300}></img> 
+      <p>Perm </p> 
+      <p>Dummy Text About perm services asdhfkasjd fsadjf asdklf askdf haksdfksad fklsda hflks adfhlskad hfklsad flka sdhflasj dfkas dhf lkasdj fhalsk dfhasl</p>
       <h3>Price 109</h3>
-      <img src='https://cdn.shopify.com/s/files/1/0024/8243/8210/collections/Shop-By_0003_Pink-Hair-Dye_1600x.png?v=1570653357' width={300}></img>
+
+      <img src='https://cdn.shopify.com/s/files/1/0024/8243/8210/collections/Shop-By_0003_Pink-Hair-Dye_1600x.png?v=1570653357' width={300}></img>  <img src='https://cdn.shopify.com/s/files/1/0024/8243/8210/collections/Shop-By_0003_Pink-Hair-Dye_1600x.png?v=1570653357' width={300}></img>  <img src='https://cdn.shopify.com/s/files/1/0024/8243/8210/collections/Shop-By_0003_Pink-Hair-Dye_1600x.png?v=1570653357' width={300}></img>  <img src='https://cdn.shopify.com/s/files/1/0024/8243/8210/collections/Shop-By_0003_Pink-Hair-Dye_1600x.png?v=1570653357' width={300}></img>
       <p>Hair Colring</p>
+      <p>Dummy Text About Hair Colring asklfbaskd asdfkl asdklf asdkl f lasdlk adslk sdjfh sad fhasd fhsakdjhfaksd f sad hfks adfjhsadk ksdf kajfkasdjhkasfdhd skf sadkf hsadk </p>
       <h3>Price 70</h3>
 
-      <img src='https://www.byrdie.com/thmb/iCg93UINT6Z-6aJKVLPthb-IXIY=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-859611700-67ec1f9196bc4dbdba38bbf120ed72a3.jpg' width={300}></img>
+      <img src='https://www.byrdie.com/thmb/iCg93UINT6Z-6aJKVLPthb-IXIY=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-859611700-67ec1f9196bc4dbdba38bbf120ed72a3.jpg' width={300}></img> <img src='https://www.byrdie.com/thmb/iCg93UINT6Z-6aJKVLPthb-IXIY=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-859611700-67ec1f9196bc4dbdba38bbf120ed72a3.jpg' width={300}></img> <img src='https://www.byrdie.com/thmb/iCg93UINT6Z-6aJKVLPthb-IXIY=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-859611700-67ec1f9196bc4dbdba38bbf120ed72a3.jpg' width={300}></img>
       <p>Shampoo</p>
+      <p>Dummy Text about Shampooing asjldfjbkasl faskld hfksadfkas hfsad jfksad  fhkas dfasl dfl as dlf aksd hf lkasjdhf sadlfa sjd fhasd f hasldl fsadj flasd fja sdflkas hdflka sdlj </p>
       <h3>Price 5</h3>
 
-      <img src='https://cdn-acpnk.nitrocdn.com/dMpdmqaVNyoDKnlvjKpSdCgrqQLGwfuY/assets/static/optimized/rev-c926924/wp-content/uploads/2014/12/What-Buzz-Cut.jpg' width={300}></img>
+      <img src='https://cdn-acpnk.nitrocdn.com/dMpdmqaVNyoDKnlvjKpSdCgrqQLGwfuY/assets/static/optimized/rev-c926924/wp-content/uploads/2014/12/What-Buzz-Cut.jpg' width={300}></img> <img src='https://cdn-acpnk.nitrocdn.com/dMpdmqaVNyoDKnlvjKpSdCgrqQLGwfuY/assets/static/optimized/rev-c926924/wp-content/uploads/2014/12/What-Buzz-Cut.jpg' width={300}></img> <img src='https://cdn-acpnk.nitrocdn.com/dMpdmqaVNyoDKnlvjKpSdCgrqQLGwfuY/assets/static/optimized/rev-c926924/wp-content/uploads/2014/12/What-Buzz-Cut.jpg' width={300}></img> <img src='https://cdn-acpnk.nitrocdn.com/dMpdmqaVNyoDKnlvjKpSdCgrqQLGwfuY/assets/static/optimized/rev-c926924/wp-content/uploads/2014/12/What-Buzz-Cut.jpg' width={300}></img>
       <p>Buzz Cut</p>
+      <p>Dummy Text about Buzzcuts adsafbaksdjbf asdf alkdflkasldk jf skld feriu hfiqe rfiq v ipqe vipe viau vais dfvop a viasd bviasd uvoqeurvo efvoas viua dvo sdvo uasdov asodvuasodv fadvu as</p>
       <h3>Price 20</h3>
 
-      <img src='https://greensalonconsulting.com/wp-content/uploads/2015/02/Complimentary-Bangs.jpg' width={300}></img>
+      <img src='https://greensalonconsulting.com/wp-content/uploads/2015/02/Complimentary-Bangs.jpg' width={300}></img> <img src='https://greensalonconsulting.com/wp-content/uploads/2015/02/Complimentary-Bangs.jpg' width={300}></img> <img src='https://greensalonconsulting.com/wp-content/uploads/2015/02/Complimentary-Bangs.jpg' width={300}></img> <img src='https://greensalonconsulting.com/wp-content/uploads/2015/02/Complimentary-Bangs.jpg' width={300}></img>
       <p>Bang Trim</p>
+      <p>Dummy Text About Bang Trim aksdfbasdf asdjf hasdlkf alsd fla ld fal lcxvdlfvjdfvierhvoeif vaopsdf sadv hasdp sad asdi vaspd vaps du vhaipu vibvi pfdvaosf vopas aps d sdapv asdpvoasudha v asds</p>
       <h3>Price 10</h3>
 
-      <img src='' width={300}></img>
-      <p></p>
-      <h3></h3>
-
-      <img src='' width={300}></img>
-      <p></p>
-      <h3></h3>
+      
+      
 
     </div>
   );
