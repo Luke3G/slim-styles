3d8df186851069c10c5a7e216af232af9d291c2a
luke3me <lucaseganter18@gmail.com>
2022-02-12 18:33:10 -0700

    added photosz

--- src/App.js
+++ src/App.js
@@ -7,12 +7,40 @@ function App() {
       <div className='container-fluid'>
         <div className='row'>
           <div className='header'>
-          <h1>Slim Styls</h1></div>
+            <h1>Slim Styls</h1></div>
         </div>
       </div>
-      <div className='body'>
-        
+      <div className='saying'>
+        <h4>At Slim Styles we are here to please customers we do many cuts, dyes, extions and much more. </h4>
       </div>
+
+      <img src='https://i.guim.co.uk/img/media/2d1b194684c17e9cbbc9b5d045f09d316bc91a88/0_132_1920_1919/master/1920.jpg?width=700&quality=85&auto=format&fit=max&s=11c74d5cb0ed244693e7d58c0251abda' width={300}></img>
+      <p>Perm </p>
+      <h3>Price 109</h3>
+      <img src='https://cdn.shopify.com/s/files/1/0024/8243/8210/collections/Shop-By_0003_Pink-Hair-Dye_1600x.png?v=1570653357' width={300}></img>
+      <p>Hair Colring</p>
+      <h3>Price 70</h3>
+
+      <img src='https://www.byrdie.com/thmb/iCg93UINT6Z-6aJKVLPthb-IXIY=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-859611700-67ec1f9196bc4dbdba38bbf120ed72a3.jpg' width={300}></img>
+      <p>Shampoo</p>
+      <h3>Price 5</h3>
+
+      <img src='https://cdn-acpnk.nitrocdn.com/dMpdmqaVNyoDKnlvjKpSdCgrqQLGwfuY/assets/static/optimized/rev-c926924/wp-content/uploads/2014/12/What-Buzz-Cut.jpg' width={300}></img>
+      <p>Buzz Cut</p>
+      <h3>Price 20</h3>
+
+      <img src='https://greensalonconsulting.com/wp-content/uploads/2015/02/Complimentary-Bangs.jpg' width={300}></img>
+      <p>Bang Trim</p>
+      <h3>Price 10</h3>
+
+      <img src='' width={300}></img>
+      <p></p>
+      <h3></h3>
+
+      <img src='' width={300}></img>
+      <p></p>
+      <h3></h3>
+
     </div>
   );
 }
